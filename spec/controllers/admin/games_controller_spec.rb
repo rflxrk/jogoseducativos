822ecb93737c208bac97dce5173826defb61dfe4
rflxrk <rafael.xrk@gmail.com>
2014-04-18 07:37:46 -0300

    games_controller_spec.rb passes tests

--- spec/controllers/admin/games_controller_spec.rb
+++ spec/controllers/admin/games_controller_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe Admin::GamesController do
   it_should_behave_like 'get success', action: :index, template: :index
   it_should_behave_like 'new action', assigns: 'game'
-  it_should_behave_like 'create action', valid: :game, invalid: :invalid_game, model: Game
+  it_should_behave_like 'create action', valid: :game_attrs, invalid: :invalid_game_attrs, model: Game
   it_should_behave_like 'edit action', valid: :game
-  it_should_behave_like 'update action', valid: :game, invalid: :invalid_game, model: Game
+  it_should_behave_like 'update action', valid: :game_attrs, invalid: :invalid_game_attrs, model: Game
   it_should_behave_like 'destroy action', valid: :game, model: Game
 end
--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -9,5 +9,15 @@ FactoryGirl.define do
     factory :invalid_game do
       puzzle nil
     end
+
+    factory :game_attrs do
+      puzzle { fixture_file_upload('spec/support/image.png') }
+      category_id { FactoryGirl.create(:category, name: Time.now.to_f).id }
+
+      factory :invalid_game_attrs do
+        puzzle nil
+        category_id 0
+      end
+    end
   end
 end
