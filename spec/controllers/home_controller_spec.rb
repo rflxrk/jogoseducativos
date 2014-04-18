3f5043501416b533ae6bafed0921bbd361eb3d1a
rflxrk <rafael.xrk@gmail.com>
2014-04-18 01:42:29 -0300

    home controller tests

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,12 +1,5 @@
 require 'spec_helper'
 
 describe HomeController do
-
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
-    end
-  end
-
+  it_should_behave_like 'get success', action: :index, template: :index
 end
