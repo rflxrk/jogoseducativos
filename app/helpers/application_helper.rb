b2e872f962df782e2422677afdd75e61d65c74ab
rflxrk <rafael.xrk@gmail.com>
2014-04-19 23:02:36 -0300

    added facebook sdk

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 module ApplicationHelper
   include BootstrapHelper
+
+  def facebook_sdk
+    %q{<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1&appId=557805640999968";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>}
+  end
 end
