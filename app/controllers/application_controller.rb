9b3ecd9bf82f664e6cbc5182e145afdcca0971e7
rflxrk <rafael.xrk@gmail.com>
2014-04-20 13:37:16 -0300

    set default host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def default_url_options
+    {host: 'jogoseducativos.github.io', port: 80}
+  end
 end
